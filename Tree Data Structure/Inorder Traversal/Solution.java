b06aab233d336d6a0beedfcc6316f7e270c67b4d
Naresh Dabburi <35326127+NareshDabburi@users.noreply.github.com>
2019-05-16 12:23:29 +0530

    Update Solution.java

--- Tree Data Structure/Inorder Traversal/Solution.java
+++ Tree Data Structure/Inorder Traversal/Solution.java
@@ -14,38 +14,19 @@
 public class Solution {
     public ArrayList<Integer> inorderTraversal(TreeNode root) {
         ArrayList<Integer> al = new ArrayList<Integer>();
-        //if (root == null) 
-            
-  
-  
         Stack<TreeNode> s = new Stack<TreeNode>(); 
         TreeNode curr = root; 
-  
-        // traverse the tree 
         while (curr != null || s.size() > 0) 
         { 
-  
-            /* Reach the left most Node of the 
-            curr Node */
             while (curr !=  null) 
             { 
-                /* place pointer to a tree node on 
-                   the stack before traversing 
-                  the node's left subtree */
                 s.push(curr); 
                 curr = curr.left; 
             } 
-  
-            /* Current must be NULL at this point */
             curr = s.pop(); 
             al.add(curr.val);
-  
-             
-  
-            /* we have visited the node and its 
-               left subtree.  Now, it's right 
-               subtree's turn */
             curr = curr.right; 
         }
         return al;
+    }
 }
