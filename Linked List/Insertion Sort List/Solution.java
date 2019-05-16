3e9cb97cc05c698a4fe8e3fa73038bd7a5179f74
Naresh Dabburi <35326127+NareshDabburi@users.noreply.github.com>
2019-05-16 12:11:35 +0530

    Create Solution.java

---
+++ Linked List/Insertion Sort List/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        ListNode current = head;
+        while(current!=null){
+            al.add(current.val);
+            current=current.next;
+        }
+        
+        int a[] = new int [al.size()];
+        for(int i=0;i<al.size();i++){
+            a[i]=al.get(i);
+        }
+        Arrays.sort(a);
+        ListNode head_new= null;
+        
+        for(int i=0;i<a.length;i++){
+            if(head_new==null){
+                ListNode newNode = new ListNode(a[i]);
+                head_new = newNode;
+                head=head_new;
+            }else{
+                ListNode newNode = new ListNode(a[i]);
+                head_new.next=newNode;
+                head_new=newNode;
+            }
+        }
+        return head;
+    }
+}
